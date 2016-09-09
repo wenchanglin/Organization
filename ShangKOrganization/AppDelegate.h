a9d86934c58c387bc0110cccc1577c9967601dab
LiHui <xiaoxiaoh99@163.com>
2016-09-09 09:54:09 +0800

    Initial Commit

---
+++ ShangKOrganization/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ShangKOrganization
+//
+//  Created by apple on 16/9/9.
+//  Copyright © 2016年 Fbw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ShangKOrganization/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ShangKOrganization
+//
+//  Created by apple on 16/9/9.
+//  Copyright © 2016年 Fbw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
